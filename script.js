8a8bbf999e057f77cb3201caca239b4736663bed
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2018-12-04 22:18:46 +0600

    Styling step 4 and modified Script.js

--- script.js
+++ script.js
@@ -6,49 +6,35 @@ let buttonNet = document.querySelector('#nav-5');
 let buttonSki = document.querySelector('#nav-6');
 let buttonSum = document.querySelector('#nav-7');
 let buttonObj = document.querySelector('#nav-8');
-let divBoxLang = document.querySelector('.box-lang');
-let divBoxEdu = document.querySelector('.box-edu');
-
-let divBoxExp = document.querySelector('.box-exp');
-
-let divBoxSoft = document.querySelector('.box-soft');
-
-let divBoxNet = document.querySelector('.box-net-skills');
-
-let divBoxSkills = document.querySelector('.box-skills');
-let divBoxSummary = document.querySelector('.box-summary');
-
-let divBoxObj= document.querySelector('.box-obj');
-
-
+let boxCorusel = document.querySelector('.corusel');
 
 buttonLang.addEventListener('click', function() {
-    divBoxLang.className = "a-1"; 
+    boxCorusel.className = "a-1"; 
 });
 
 buttonEdu.addEventListener('click', function() {
-    divBoxEdu.className = "a-2"; 
+    boxCorusel.className = "a-2"; 
  });
 
 buttonExp.addEventListener('click', function() {
-    divBoxExp.className = "a-3"; 
+    boxCorusel.className = "a-3"; 
 });
 
 buttonSoft.addEventListener('click', function() {
-    divBoxSoft.className = "a-4"; 
+    boxCorusel.className = "a-4"; 
 });
 
 buttonNet.addEventListener('click', function() {
-    divBoxNet.className = "a-5"; 
+    boxCorusel.className = "a-5"; 
 });
 
 buttonSki.addEventListener('click', function() {
-    divBoxSkills.className = "a-6"; 
+    boxCorusel.className = "a-6"; 
 });
 
 buttonSum.addEventListener('click', function() {
-    divBoxSummary.className = "a-7"; 
+    boxCorusel.className = "a-7"; 
 });
 
-buttonObj.addEventListener('click', function() {      divBoxObj.className = "a-8"; 
+buttonObj.addEventListener('click', function() {      boxCorusel.className = "a-8"; 
 });
